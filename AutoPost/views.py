28d950d87c2a37a2f2e46e71c5aa90dcd459390e
Ashwin Iyer <ashwiniyer1706@gmail.com>
2022-01-24 19:29:29 +0530

    Cleaner functions for MessageForm

--- AutoPost/views.py
+++ AutoPost/views.py
@@ -1,9 +1,29 @@
-import tweepy
-
+import datetime
 from django.shortcuts import redirect, render
 from . import TweetBot
 from .forms import MessageForm
 # Create your views here.
+
+
+def hasDatePassed(date):
+    '''
+    date(str): String of the format YYYY-MM-DD
+    '''
+    year, month, day = date.split('-')
+    if(datetime.datetime.now().date() > datetime.date(int(year), int(month), int(day))):
+        return True
+    return False
+
+
+def hasTimePassed(time):
+    '''
+    time(str): String of the format HH:MM (24 hr format)
+    '''
+    hour, minute = time.split(":")
+    print(datetime.datetime.now().time().hour, hour)
+    if(datetime.datetime.now().time().hour > int(hour)):
+        return True
+    return False
 
 
 def index(request):
@@ -11,7 +31,11 @@ def index(request):
         form = MessageForm(request.POST)
         content = None
         if form.is_valid():
-            print(request.POST)
+            # print(request.POST)
+            date_published = request.POST['date_published']
+            time_published = request.POST['time_published']
+            message = request.POST['message']
+            print(date_published, "-", time_published, "-", message)
         # if content:
         #     print("Content:", content)
         #     bot = TweetBot.TwitterAPI()
