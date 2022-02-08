b2027dd30fb6fef59efd08fa10a2b082655bcf34
Ashwin Iyer <ashwiniyer1706@gmail.com>
2022-02-08 16:27:03 +0530

    Update database schema

--- AutoPost/forms.py
+++ AutoPost/forms.py
@@ -6,43 +6,4 @@ import datetime
 class MessageForm(ModelForm):
     class Meta:
         model = models.Message
-        fields = ['message', 'author', 'date_published', 'time_published']
-        widgets = {
-            'date_published': widgets.DateInput(attrs={'type': 'date'}),
-            'time_published': widgets.DateInput(attrs={'type': 'time'})
-        }
-
-    def hasDatePassed(self, date):
-        '''
-        date(datetime.date): format YYYY-MM-DD
-        '''
-        if(datetime.datetime.now().date() > date):
-            return True
-        return False
-
-    def hasTimePassed(self, time):
-        '''
-        time(datetime.time): format HH:MM (24 hr format)
-        '''
-        if(datetime.datetime.now().time().hour < time.hour):
-            return False
-        elif(datetime.datetime.now().time().hour == time.hour):
-            if(datetime.datetime.now().time().minute <= time.minute):
-                return False
-            else:
-                return True
-        else:
-            return True
-
-    def clean(self):
-        super().clean()
-        cleaned_data = self.cleaned_data
-        if len(cleaned_data.keys()) != 3:
-            raise ValidationError("Incomplete Form")
-        else:
-            if self.hasDatePassed(cleaned_data['date_published']):
-                raise ValidationError("Date has already passed")
-            elif self.hasTimePassed(cleaned_data['time_published']) and cleaned_data['date_published'] == datetime.datetime.now().date():
-                raise ValidationError("TIme has passed")
-            else:
-                return cleaned_data
+        fields = ['message', 'author']
--- AutoPost/models.py
+++ AutoPost/models.py
@@ -9,9 +9,10 @@ from django.utils import timezone
 class Message(models.Model):
     message = models.TextField("Message")
     author = models.CharField("Author", default="Anonymous", max_length=50)
-    date_published = models.DateField(default=timezone.now().date())
-    time_published = models.TimeField(default=timezone.now().time())
     sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.message + " - " + self.author
 
     def toggle_message_status(self):
         self.sent = True
